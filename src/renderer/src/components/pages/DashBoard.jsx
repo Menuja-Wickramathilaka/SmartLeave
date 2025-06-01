a04408c5fb13c8c97b426cfa9a6259a34a461c68
Yushan Hettiarachchi <130626574+Dismi343@users.noreply.github.com>
2025-06-02 01:21:35 +0530

    DashBoard patch-1

--- src/renderer/src/components/pages/DashBoard.jsx
+++ src/renderer/src/components/pages/DashBoard.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+import LogoConmponent from "../components/Logo";
+import LogoutIcon from '@mui/icons-material/Logout';
 function DashBoardPage(){
 
     const backonClick=(e)=>{
@@ -7,9 +9,26 @@ function DashBoardPage(){
     }
     return(
         <>
-        <button onClick={backonClick}>
-            back
+
+        <div className="flex flex-col  items-center h-screen bg-[url(../assets/images/background.jpg)] bg-cover bg-center   ">
+        <div className="z-10 h-15 w-full bg-[#D9D9D9] shadow-md flex items-center justify-end px-10">  
+            <p className="px-10">Last sync: 2 days ago</p> 
+            <button className="text-white rounded-full bg-[#70A9A1] w-30 h-10  ">Sync</button>
+        </div>
+        <div className="absolute inset-0 bg-black/50"></div> 
+
+
+        <div className="z-10 flex w-full justify-end px-10">
+            <button onClick={backonClick} className="text-white  rounded-full bg-[#70A9A1] w-35 h-12 mt-8 hover:bg-red-800">
+           <LogoutIcon /> Logout
         </button>
+        </div>
+       <LogoConmponent />
+       <div className="border border-white w-full h-96 flex  z-10 mt-20">
+        
+        </div> 
+        </div>
+       
         </>
     );
 }
--- src/renderer/src/components/pages/LoginPage.jsx
+++ src/renderer/src/components/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { useNavigate } from 'react-router-dom';
-import logo from '../../assets/images/Scl_Logo.png';
 import { useState } from 'react';
+import Logo from '../components/Logo';
 
 function LoginPage() {
 
@@ -31,14 +31,7 @@ function LoginPage() {
         <div className="flex flex-col  items-center h-screen bg-[url(../assets/images/background.jpg)] bg-cover bg-center lg:pt-30 pt-20  ">
               <div className="absolute inset-0 bg-black/50"></div> 
               
-              <div className="flex  w-[50vh] lg:w-[70vh] h-[15vh]  z-10 justify-between items-center pt-10 ">
-                <div className="w-1/2  items-center flex">
-                    <img src={logo} alt="" className='w-40 lg:w-50'/>
-                </div>
-                 <div className="w-1/2 justify-center items-center flex pr-12">
-                    <p className="text-white font-sans text-4xl font-semibold lg:text-5xl text-shadow-lg/30">SmartLeave</p>
-                </div>
-              </div>
+         <Logo/>
 
               <div className='flex bg-[#CFE0C3]/75 border  rounded-2xl shadow-lg p-4 lg:p-10 z-10 w-[60vh] h-[40vh] shadow-lg/30 flex-col items-center mt-30'>
                 <h1 className='text-3xl font-semibold'>LOGIN</h1>
@@ -46,21 +39,21 @@ function LoginPage() {
                     <form className="grid grid-flow-col grid-rows-3 gap-6" onSubmit={handleLogin}>
                         <div >
                             <input type="text" 
-                            className='border border-white rounded-full bg-white shadow-lg/30 w-80 px-3 h-8' 
+                            className='border border-white rounded-full bg-white shadow-lg/30 w-80 px-3 h-8 focus:outline-none ' 
                             placeholder='UserName'
                             onChange={(e)=>setUserName(e.target.value)} />
                         </div>
                         
                         <div >
                             <input type="password" 
-                            className='border border-white rounded-full bg-white shadow-lg/30 w-80 px-3 h-8' 
+                            className='border border-white rounded-full bg-white shadow-lg/30 w-80 px-3 h-8 focus:outline-none' 
                             placeholder='Password'
                             onChange={(e)=>{setPassword(e.target.value)}}/>
                         </div>
                        
                         
                         <div className='flex justify-center items-center ' >
-                            <button type="submit" className='bg-[#1F363D] text-white px-10 py-1  rounded-full shadow-lg/30'  >
+                            <button type="submit" className='bg-[#1F363D] text-white px-10 py-1  rounded-full shadow-lg/30 focus:outline-none hover:bg-black'  >
                                 Login
                             </button>
                         </div>
